ef6ffd89fdd31b81a68505e6b0b7ab8dbfdbf3a2
Omar <44298100+omards8@users.noreply.github.com>
2020-09-23 18:11:20 +0200

    AE1-2

---
+++ ae1_2.js
@@ -0,0 +1,8 @@
+var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+for (let index = 0; index < meses.length; index++) {
+    console.log(meses[index]);
+    
+}
+
+console.table(meses);
+
